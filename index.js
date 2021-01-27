950e0c676c33ee937b7201f61552c4fe171e500e
BRvanHogen <brvanhogen@hotmail.nl>
2021-01-27 15:59:53 +0100

    removed solution, ha, ha, ha

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-//write me!
-
 function moneyMultiplierOrNot (num1, num2) {
-    const result = Math.pow(num1, num2) / num2;
-    console.log("result: ", result);
-    return result;
+  //write me!
 }
 
 module.exports = {
